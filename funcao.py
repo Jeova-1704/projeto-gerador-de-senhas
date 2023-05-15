1a75ecd1bb2b688ffbf9e6b9f92f8cee8dc32113
Jeova BL <jeovab115@gmail.com>
2023-05-14 22:57:16 -0300

    os códigos da funcionalidade de gerar senha, avaliar nivel de segutança e de copiar, tudo comentado

--- funcao.py
+++ funcao.py
@@ -1,113 +1,128 @@
+# Codigo feito com o POO para facilitar a execução e tratar o problema de uma forma mais simples 
+
+# Importando da bliblioteca "Strings" as 'variables' respectivas a letras minusculas, letras maisculas, numeros e caracteres especiais
 from string import ascii_lowercase, ascii_uppercase, digits, punctuation
-import random
+# Importando o metodo choices que embaralha a senha com varios caracteres diferentes para tornala mais segura
+from random import choices
+# importando uma bliblioteca para copiar a senha para a área de tranferencia
 import pyperclip
 
-class GeradorSenhas:
-    def __init__(self, num_caracteres, letras_maiusculas, letras_minusculas, numeros, simbolos):
-        self.num_caracteres = num_caracteres
-        self.letras_maiusculas = letras_maiusculas
-        self.letras_minusculas = letras_minusculas
-        self.numeros = numeros
-        self.simbolos = simbolos
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
+# Código da parte de POO para criar um codigo que faça a senha, avalie o nivel de seguraça dela e cópie para a área de segurança
+
+#Classe gerador onde fica resposnsavel por toda a parte da senha, avaliação e copia
+class GeradorSenhas:
+    #recebe TRUE ou FALSE para avaliar se é ou não para gerar com as letras maisculas e minusculas, numeros e simbolos 
+    def __init__(self, num_caracteres, letras_maiusculas, letras_minusculas, numeros, simbolos):
+        self.__num_caracteres = num_caracteres
+        self.__letras_maiusculas = letras_maiusculas
+        self.__letras_minusculas = letras_minusculas
+        self.__numeros = numeros
+        self.__simbolos = simbolos
+
+# Função que avalia os TRUE e FALSE para gerar a senha de acordo com as especificações acima que foram passadas e no final gera a senha que é retornada
     def gerar_senha(self):
         combinacao = ""
-        if self.letras_maiusculas:
+        if self.__letras_maiusculas:
             combinacao += ascii_uppercase
     
-        if self.letras_minusculas:
+        if self.__letras_minusculas:
             combinacao += ascii_lowercase
     
-        if self.numeros:
+        if self.__numeros:
             combinacao += digits
 
-        if self.simbolos:
+        if self.__simbolos:
             combinacao += punctuation
 
-        senha = ''.join(random.choices(combinacao, k=self.num_caracteres))
+        senha = ''.join(choices(combinacao, k=self.__num_caracteres))
 
         return senha
 
+# Avalia o nivel de segurança de uma senha baseada em um sistema de pontuação de 0 a 10, onde 10 é uma senha muito boa e 0 uma senha facil de ser quebrada
+#Sitema funciona avaliando em ordem: o tamanho da senha -> se ela tem letras minusculas -> se ela tem letras maisculas -> se ela tem caracteres especiais -> se tem números
     def avaliar_seguranca(self, senha):
-        self.pontos = 0
+        self.__pontos = 0
 
         if len(senha) <= 5:
-            self.pontos += 0.5
+            self.__pontos += 0.5
             for carac in senha:
                 if carac in ascii_lowercase:
-                    self.pontos += 1
+                    self.__pontos += 1
                     break
             for carac in senha:
                 if carac in ascii_uppercase:
-                    self.pontos += 1
+                    self.__pontos += 1
                     break
 
             for carac in senha:
                 if carac in punctuation:
-                    self.pontos += 2
+                    self.__pontos += 2
                     break
 
             for carac in senha:
                 if carac in digits:
-                    self.pontos += 2
+                    self.__pontos += 2
                     break
 
         elif 5 < len(senha) <= 10:
-            self.pontos += 1
+            self.__pontos += 1
             for carac in senha:
                 if carac in ascii_lowercase:
-                    self.pontos += 2
+                    self.__pontos += 2
                     break
             for carac in senha:
                 if carac in ascii_uppercase:
-                    self.pontos += 2
+                    self.__pontos += 2
                     break
             for carac in senha:
                 if carac in punctuation:
-                    self.pontos += 2
+                    self.__pontos += 2
                     break
             for carac in senha:
                 if carac in digits:
-                    self.pontos += 2
+                    self.__pontos += 2
                     break
 
         elif len(senha) > 10:
-            self.pontos += 2
+            self.__pontos += 2
             for carac in senha:
                 if carac in ascii_lowercase:
-                    self.pontos += 2
+                    self.__pontos += 2
                     break
             for carac in senha:
                 if carac in ascii_uppercase:
-                    self.pontos += 2
+                    self.__pontos += 2
                     break
 
             for carac in senha:
                 if carac in punctuation:
-                    self.pontos += 2
+                    self.__pontos += 2
                     break
 
             for carac in senha:
                 if carac in digits:
-                    self.pontos += 2
+                    self.__pontos += 2
                     break
 
-        if self.pontos <= 5:
+        if self.__pontos <= 5:
             return 'Senha Fraca'
-        elif 5 < self.pontos <= 7:
+        elif 5 < self.__pontos <= 7:
             return 'Senha Intermediaria'
-        elif 7 < self.pontos:
+        elif 7 < self.__pontos:
             return 'Senha Forte'
         
+# Função simples onde copia a varivel de senha onde foi guardada a senha gerada e deixa na área de tranferencia
     def copiar(self):
         pyperclip.copy(senha)
     
 
 
-
+# Esse código abaixo é um codigo de teste onde ele só funciona se for executado esse arquiov, é uma forma de testar para ver se os comandos acima estão funcionando corretamente
 if __name__ == '__main__':
     # uso da classe
-    gerador = GeradorSenhas(num_caracteres=4, letras_maiusculas=True, letras_minusculas=True, numeros=False, simbolos=False)
+    gerador = GeradorSenhas(num_caracteres=7, letras_maiusculas=True, letras_minusculas=True, numeros=True, simbolos=False)
     senha = gerador.gerar_senha()
     print(f'Senha gerada: {senha}')
     nivel_seguranca = gerador.avaliar_seguranca(senha)
