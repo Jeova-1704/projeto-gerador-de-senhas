226370415a22d0129edab84a6751d1882e7c8772
Pierre Monteiro <marcospierremonteiro3@gmail.com>
2023-05-15 22:48:08 -0300

    Adicionando_variáveis

--- Front.py
+++ Front.py
@@ -1,6 +1,6 @@
 
 # Importando Backend 
-import funcao
+import back
 # Importando PySimpleGui
 import PySimpleGUI as sg
 
@@ -21,20 +21,29 @@ def tela_principal():
     sg.theme('DarkGrey11')
     layout=[
         [sg.HSeparator(),sg.Text('Gerador de Senha',font='Helvetica 16 bold'),sg.HSeparator()],
-        [sg.Text(f'Olá {nome_usuario}',font='Helvetica 12 bold',text_color='LightBlue')],
-    ]
-    return sg.Window('Tela Principal', layout=layout, element_justification='c',size=(500,550), finalize=True)
-
-
-
+        [sg.Text(f'Olá {nome_usuario} !',font='Helvetica 12 bold',text_color='LightBlue')],
+        [sg.Text(' ')],
+        [sg.Text('Quantidade de caracteres:',font=("Helvetica 12")),sg.Slider(range=(1, 50), orientation='h', size=(20,20), default_value=50,key=('-SLIDER-'))],
+        [sg.Text('')],
+        [sg.Checkbox('Letras Maiúsculas (ABC)',key="-MAIUSC-")],
+        [sg.Checkbox('Letras Minúsculas (abc)',key="-MINUSC-")],
+        [sg.Checkbox('Números (123)',key="-NUM-")],
+        [sg.Checkbox('Caracteres Especiais (!@#)',key="-CHAR-")],
+        [sg.Button('Gerar Senha',font=('Helvetica 12 bold'))],
+        [sg.Text('')],
+        [sg.Text('',size=(14,1)),sg.Text(f'{senha_gerada}',font=('Helvetica 14 bold'),),sg.Button('Copiar Senha')],
+        [sg.Text('',size=(15,1)),sg.Text('Senha Forte',text_color='Green')],
+        
+        ]
+    return sg.Window('Tela Principal', layout=layout, element_justification='l',size=(500,550), finalize=True)
 
 janela_login , janela_principal = tela_login(),None
 
 
+senha_gerada = " "
 
 while True:
     Window, event, values = sg.read_all_windows()
-
     if event == sg.WIN_CLOSED:
         break
 
@@ -44,6 +53,24 @@ while True:
         janela_principal = tela_principal()
         janela_principal.un_hide()
         
+    if event == 'Gerar Senha':
 
+        # adicionar resultado da senha aqui
+        senha_gerada = "senha"
+        
+        # Retorno das checkbox ( True / False )
+        slider_quantidade_caracteres = values['-SLIDER-']
+        checkbox_maiusculo = values['-MAIUSC-']
+        checkbox_minusculo = values['-MINUSC-']
+        checkbox_numero = values['-NUM-']
+        checkbox_caractere = values['-CHAR-']
+
+        janela_principal.hide()
+        janela_principal.un_hide()
+
+    # O QUE ACONTECE AO CLICAR NO BOTÃO COPIAR SENHA 
+    if event == "Copiar Senha":
+        back.copiar(senha_gerada)
+        sg.popup_ok("Senha Copiada com sucesso ✔!")
 
 
