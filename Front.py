e9ed2abd334467cfc6e7dd7e7f0eb17056c9a249
Pierre Monteiro <marcospierremonteiro3@gmail.com>
2023-05-15 21:37:55 -0300

    Teste02

--- Front.py
+++ Front.py
@@ -6,14 +6,30 @@ import PySimpleGUI as sg
 
 
 def tela_login():
-    sg.theme('Reddit')
+    sg.theme('DarkGrey11')
     layout=[
-    [sg.HSeparator(),sg.Text('Tela de Login',font='Helvetica 16 bold'),sg.HSeparator()],   
+    [sg.HSeparator(),sg.Text('Tela de Login',font='Helvetica 16 bold'),sg.HSeparator()],
+    [sg.Text("",size=(10,5))],
+    [sg.Text("Usuário :",font="Helvetica 15 bold"),sg.InputText(" ",size=(20,1),key='-usuario-')],
+    [sg.Text("Senha :",font="Helvetica 15 bold"),sg.InputText(" ",size=(20,1))],
+    [sg.Text("",size=(10,2))],
+    [sg.Button("Entrar",font="helvetica 10 bold"),sg.Button("Sair",button_color="Red",font="helvetica 10 bold",size=(5,1))],
     ]
-    return sg.Window('Tela Login', layout=layout, element_justification='l',size=(500,550), finalize=True)
+    return sg.Window('Tela Login', layout=layout, element_justification='c',size=(500,350), finalize=True)
 
-janela_login = tela_login()
+def tela_principal():
+    sg.theme('DarkGrey11')
+    layout=[
+        [sg.HSeparator(),sg.Text('Gerador de Senha',font='Helvetica 16 bold'),sg.HSeparator()],
+        [sg.Text(f'Olá {nome_usuario}',font='Helvetica 10 bold')],
+    ]
+    return sg.Window('Tela Principal', layout=layout, element_justification='c',size=(500,550), finalize=True)
 
+
+
+
+janela_login , janela_principal = tela_login(),None
+nome_usuario = Values['-usuario-']
 
 
 while True:
@@ -22,6 +38,11 @@ while True:
     if event == sg.WIN_CLOSED:
         break
 
+    if event == "Entrar":
+        janela_login.hide()
+        janela_principal = tela_principal()
+        janela_principal.un_hide()
+        
 
 
 
